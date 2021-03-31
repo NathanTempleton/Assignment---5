55977e4c36994f27edd498c650e505a474a9a573
NathanTempleton <nathan.templeton@ocsbstudent.ca>
2021-03-31 17:55:28 +0000

    Fixed some errors

--- script.js
+++ script.js
@@ -6,24 +6,25 @@ let diameter = 0
 let area = 0
 let circumference = 0
 let answer = 0
+let playerChoice = ''
 
 function setup () {
   playerChoice = document.getElementById('player-choice').value
-  if (playerChoice.toLowerCase() == 'area') {
+  if (playerChoice.toLowerCase() === 'area') {
     document.getElementById('text').innerHTML = 'Now enter your diameter.'
     document.getElementById('player-choice').style.display = 'none'
     document.getElementById('player-diameter').style.display = 'inline'
     document.getElementById('button').style.display = 'none'
     document.getElementById('button-2').style.display = 'inline'
-} else if (playerChoice.toLowerCase() == 'circumference') {
+  } else if (playerChoice.toLowerCase() === 'circumference') {
     document.getElementById('text').innerHTML = 'Now enter your diameter.'
     document.getElementById('player-choice').style.display = 'none'
     document.getElementById('player-diameter').style.display = 'inline'
     document.getElementById('button').style.display = 'none'
     document.getElementById('button-2').style.display = 'inline'
-} else {
+  }  else {
   document.getElementById('result').innerHTML = 'Error'
-}
+  }
 }
 
 function math () {
@@ -55,8 +56,8 @@ function math () {
   document.getElementById('result').innerHTML = 'Error'
   document.getElementById('text').innerHTML = 'Would you like to calculate the area or circumference of the circle?'
   document.getElementById('player-choice').style.display = 'inline'
-    document.getElementById('player-diameter').style.display = 'none'
-    document.getElementById('button').style.display = 'inline'
-    document.getElementById('button-2').style.display = 'none'
+  document.getElementById('player-diameter').style.display = 'none'
+  document.getElementById('button').style.display = 'inline'
+  document.getElementById('button-2').style.display = 'none'
 }
 }
